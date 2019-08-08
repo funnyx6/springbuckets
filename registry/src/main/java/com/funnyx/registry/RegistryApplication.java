a7949df2892a926f0e7f646bf239afa6c592e055
funnyx <329284843@qq.com>
2019-08-08 15:57:05 +0800

    配置文件修改

---
+++ registry/src/main/java/com/funnyx/registry/RegistryApplication.java
@@ -0,0 +1,12 @@
+package com.funnyx.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RegistryApplication.class, args);
+  }
+}
