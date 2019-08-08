f95e33c7166004e433344bc3ea561372e78df050
funnyx <329284843@qq.com>
2019-08-08 13:29:03 +0800

    添加spring-nacos孵化器版本

---
+++ spring-nacos/src/main/java/com/funnyx/SpringNacosApplication.java
@@ -0,0 +1,12 @@
+package com.funnyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNacosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringNacosApplication.class, args);
+  }
+}
---
+++ spring-nacos/src/test/java/com/funnyx/SpringNacosApplicationTests.java
@@ -0,0 +1,14 @@
+package com.funnyx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringNacosApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
