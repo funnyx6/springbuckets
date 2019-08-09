278afaa433256e3d254f089228a641a5b1e3a726
funnyx <329284843@qq.com>
2019-08-09 09:00:23 +0800

    修改配置

--- user/src/main/java/com/funnyx/user/UserApplication.java
+++ user/src/main/java/com/funnyx/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.funnyx.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class UserApplication {
