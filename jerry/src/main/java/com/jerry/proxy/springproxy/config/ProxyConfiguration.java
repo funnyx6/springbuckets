38416784bb2d6567a00eba8e59872010626d2ea1
funnyx <329284843@qq.com>
2019-09-04 22:24:25 +0800

    add comment

--- jerry/src/main/java/com/jerry/proxy/springproxy/config/ProxyConfiguration.java
+++ jerry/src/main/java/com/jerry/proxy/springproxy/config/ProxyConfiguration.java
@@ -7,6 +7,15 @@ import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Spring提供的动态代理的方式，这里以配置的形式来实现动态代理，主要是由这个ProxyFactoryBean代理工厂实现
+ *
+ * @see org.springframework.aop.framework.AopProxy
+ * @see org.springframework.aop.framework.AopProxyFactory
+ * @see org.springframework.aop.framework.ProxyFactory
+ * @see ProxyFactoryBean
+ *     <p>这里最基础的接口就是AopProxy，它的实现有两种，一个是Java的动态代理，另一个就是CGlib
+ */
 @Configuration
 public class ProxyConfiguration {
 
