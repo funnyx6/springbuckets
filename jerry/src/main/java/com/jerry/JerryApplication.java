7a4a0d556b71998b9f4c7102e9976daaf923c988
funnyx <329284843@qq.com>
2019-09-04 15:32:12 +0800

    添加新模块，readme modify

---
+++ jerry/src/main/java/com/jerry/JerryApplication.java
@@ -0,0 +1,12 @@
+package com.jerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JerryApplication.class, args);
+  }
+}
