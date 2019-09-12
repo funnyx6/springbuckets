d9ce57e7387f1f751810f04dfdacb7cc0c264223
funnyx <329284843@qq.com>
2019-09-12 13:47:08 +0800

    添加Spring Cache

--- order/src/main/java/com/funnyx/OrderApplication.java
+++ order/src/main/java/com/funnyx/OrderApplication.java
@@ -2,12 +2,14 @@ package com.funnyx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableCaching
 public class OrderApplication {
 
   public static void main(String[] args) {
--- order/src/main/java/com/funnyx/order/service/OrderService.java
+++ order/src/main/java/com/funnyx/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.funnyx.order.service;
 import com.funnyx.order.entity.Order;
 import com.funnyx.order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,6 +17,8 @@ public class OrderService {
     return orderRepository.save(order);
   }
 
+  // 添加方法级别注解，如果是类级别注解，则只对public的方法有效。
+  @Cacheable(cacheNames = "orders")
   public Optional<Order> findById(Long id) {
     return orderRepository.findById(id);
   }
