ef620db915bcfe5df9b78e690a77e24e1657926f
funnyx <329284843@qq.com>
2019-09-19 21:36:35 +0800

    翻译提交

--- jerry/src/main/java/com/jerry/springcore/starter/MainStarter.java
+++ jerry/src/main/java/com/jerry/springcore/starter/MainStarter.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class MainStarter {
 
   public static void main(String[] args) {
+
     AnnotationConfigApplicationContext context =
         new AnnotationConfigApplicationContext(MainConfig.class);
 
